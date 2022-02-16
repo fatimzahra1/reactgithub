854a93e973315f14ce858b7c6cc476972d2519a7
fatima ezzahraa el <fatimzahraelmadini@gmail.com>
2022-02-17 00:24:20 +0100

    Retrieve data for user

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ class App extends Component {
    this.setState({users:res.data.items ,loading: false})
   }
   getUser = async (username) => {
+    
     this.setState({ loading: true})
-    const res =await axios.get(`https://api.github.com/users?q=${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res =await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
    this.setState({user:res.data ,loading: false})
+   
   }
   clearUsers = () => this.setState({users:[], loading:false})
   setAlert = (msg, type) => {
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function User(props) {
+  const User = ({user, loading, getUser})=> {
     const { login } = useParams();
-    console.log({login})
+          useEffect(async () => {
+             await getUser(login)
+            
+          }, []);
+
+          const {
+              name,
+              avatar_url,
+              location,
+              bio,
+              blog,
+              html_url,
+              followers,
+              following,
+              public_repos,
+              public_gists,
+              hireable
+          }=user
+    
     return (
         <div>
-         { login} 
+        {name}
         </div>
     );
 }
