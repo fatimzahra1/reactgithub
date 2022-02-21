6cc799ac182bae8f67867821f3f0bb3185d5ee86
fatima ezzahraa el <fatimzahraelmadini@gmail.com>
2022-02-21 16:19:15 +0100

    Add link to the user website

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
   const User = ({user, loading, getUser})=> {
     const { login } = useParams();
@@ -14,6 +16,7 @@ import { useEffect, useState } from "react";
               avatar_url,
               location,
               bio,
+              company,
               blog,
               html_url,
               followers,
@@ -22,11 +25,49 @@ import { useEffect, useState } from "react";
               public_gists,
               hireable
           }=user
-    
-    return (
-        <div>
-        {name}
+    if (loading) return <Spinner />
+    const st={blog}
+        return (
+        <Fragment>
+        <Link to='/' className='btn btn-light'> Back to search
+        </Link>
+        hireable: {' '}
+        {hireable ? (<i className='fas fa-check text-success'/>): (<i className='fas fa-times-circle text-danger'/>)}
+        <div className='card grid-2'>
+            <div className='all-center'>
+             <img src={avatar_url} className='round-img' alt='' style={{ width: '150px'}}/>
+             <h1>{name}</h1>
+             <p>Location: {location}</p>
         </div>
+        <div>
+            {bio && (<Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+            </Fragment>)}
+            <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+            <ul>
+                <li>
+                    {company && <Fragment>
+                       <strong>Company: </strong> {company}
+                    </Fragment>}
+                </li>
+
+                <li>
+                    {blog && <Fragment>
+                       <strong>Website: </strong> <a href={blog}>{blog}</a>
+                    </Fragment>}
+                </li>
+            </ul>
+        </div>
+        </div>
+        <div className='card text-center'>
+         <div className='badge badge-primary'>Followers: {followers} </div>
+         <div className='badge badge-success'>Following: {following} </div>
+         <div className='badge badge-light'>Public Repos: {public_repos} </div>
+         <div className='badge badge-dark'>Public Gists: {public_gists} </div>
+
+        </div>
+        </Fragment>
     );
 }
 
